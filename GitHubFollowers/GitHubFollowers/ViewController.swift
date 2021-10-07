4cc1e49f7d1d4124516f4bf496a728e79f1e7cfc
Jakob Haslam <jakehaslam38@gmail.com>
2021-10-07 14:16:00 -0600

    initial set up complete

---
+++ GitHubFollowers/GitHubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Jake Haslam on 10/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
